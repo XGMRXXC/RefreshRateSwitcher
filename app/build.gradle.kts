708372cf59fa40ed1a2efe9f896c5c0806c8be10
XGMRXXC <65719560+XGMRXXC@users.noreply.github.com>
2026-09-19 15:22:17 +0800

    chore: app/build.gradle.kts (v4.2.1.6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.dsh.refreshswitch"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4215
-        versionName = "4.2.1.5"
+        versionCode = 4216
+        versionName = "4.2.1.6"
     }
 
     signingConfigs {
